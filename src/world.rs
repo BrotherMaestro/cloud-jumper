49650dc8dc8293120ae2cef7d96bde648241ae9e
Hayden Sip <hsipper@icloud.com>
2025-05-23 10:27:57 +1000

    Update to bevy 0.16 and get code compiling. Note, Shader broke

--- src/world.rs
+++ src/world.rs
@@ -1,6 +1,5 @@
 use bevy::ecs::component::*;
 use bevy::prelude::*;
-use bevy::sprite::MaterialMesh2dBundle;
 use bevy::window::PrimaryWindow;
 use compute::distributions::Distribution;
 use compute::prelude::Normal;
@@ -10,11 +9,23 @@ use crate::cloud_material::CloudMaterial;
 use crate::region::Region;
 use crate::region_set::RegionSet;
 
+// COLOURS FOR PLATFORMING WORLD
+const SKY_BLUE: Color = Color::srgb(0.53, 0.81, 0.92);
+const DARK_GRAY: Color = Color::srgb(0.14, 0.14, 0.14);
+const GRASS: Color = Color::srgb(0.04, 0.69, 0.32);
+
+// PLATFORM SPAWN CONSTANTS
+const MIN_PLATFORM_DISTANCE: f64 = 250.0;
+const MAX_PLATFORM_DISTANCE: f64 = 2500.0;
+
+// MOVEMENT CONSTANTS
+const CAMERA_SCROLL_SPEED: f32 = 50.0;
+
 pub struct WorldPlugin;
 
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup)
+        app.insert_resource(ClearColor(DARK_GRAY)).add_systems(Startup, setup)
             .add_systems(Update, (scroll_camera, despawn_platform, spawn_platform));
     }
 }
@@ -26,45 +37,26 @@ pub struct WorldCamera;
 #[derive(Component)]
 pub struct Ground;
 
-// COLOURS FOR PLATFORMING WORLD
-const SKY_BLUE: Color = Color::rgb(0.53, 0.81, 0.92);
-const DARK_GRAY: Color = Color::rgb(0.14, 0.14, 0.14);
-
-// PLATFORM SPAWN CONSTANTS
-const MIN_PLATFORM_DISTANCE: f64 = 250.0;
-const MAX_PLATFORM_DISTANCE: f64 = 2500.0;
-
-// MOVEMENT CONSTANTS
-const CAMERA_SCROLL_SPEED: f32 = 50.0;
 
 pub fn setup(mut commands: Commands, window: Query<&Window, With<PrimaryWindow>>) {
-    let window = window.get_single().unwrap();
+    let window = window.single().unwrap();
     let width = window.width();
     let height = window.height();
     let sky_size = 1.2 * Vec2::new(width, height);
 
-    let sky_bundle = SpriteBundle {
-        sprite: Sprite {
+    let sky_sprite = Sprite {
             color: SKY_BLUE,
             custom_size: Some(sky_size),
             ..default()
-        },
-        ..default()
     };
-
+    
     commands
         .spawn((
-            Camera2dBundle {
-                camera: Camera {
-                    clear_color: ClearColorConfig::Custom(DARK_GRAY),
-                    ..default()
-                },
-                ..default()
-            },
+            Camera2d, 
             WorldCamera,
         ))
         .with_children(|parent| {
-            parent.spawn(sky_bundle);
+            parent.spawn(sky_sprite);
         });
 
     let ground_width = 120000.0;
@@ -72,19 +64,16 @@ pub fn setup(mut commands: Commands, window: Query<&Window, With<PrimaryWindow>>
 
     // Spawn Ground after Sky (render above)
     commands.spawn((
-        SpriteBundle {
-            sprite: Sprite {
-                color: Color::YELLOW_GREEN,
+            (Sprite {
+                color: GRASS,
                 custom_size: Some(Vec2::new(ground_width, ground_height)),
                 ..default()
             },
-            transform: Transform::from_translation(Vec3::new(
+            Transform::from_translation(Vec3::new(
                 0.0,
                 (-ground_height / 2.0) - (height / 4.0),
                 1.0,
-            )),
-            ..default()
-        },
+            ))),
         Ground,
     ));
 }
@@ -95,7 +84,7 @@ pub fn scroll_camera(
 ) {
     for (_, mut transform) in &mut q_camera {
         let local_y = transform.local_y();
-        transform.translation += local_y * CAMERA_SCROLL_SPEED * time.delta_seconds();
+        transform.translation += local_y * CAMERA_SCROLL_SPEED * time.delta_secs();
     }
 }
 
@@ -104,7 +93,7 @@ pub fn despawn_platform(
     q_ground: Query<(Entity, &Sprite, &Transform), With<Ground>>,
     q_camera: Query<(&Camera, &GlobalTransform), With<WorldCamera>>,
 ) {
-    let (camera, camera_transform) = q_camera.single();
+    let Ok((camera, camera_transform)) = q_camera.single() else { todo!() };
     for (ground_id, ground_sprite, ground_transform) in q_ground.iter() {
         let ground_y_offset = if let Some(size) = ground_sprite.custom_size {
             size.y / 2.0
@@ -140,8 +129,8 @@ pub fn spawn_platform(
 
     // Use global transforms to ensure the positions are relative (and therefore measure valid distances)
     // Our single player game has 1 WorldCamera, and 1 window
-    let window = q_window.single();
-    let camera_translation = q_camera.single().translation();
+    let Ok(window) = q_window.single() else { todo!() };
+    let camera_translation = q_camera.single().expect("ONLY 1 CAMERA IN GAME SO FAR").translation();
     let top_of_camera = camera_translation.y + window.height() / 2.0;
     let window_half_width = window.width() / 2.0;
     let left_of_camera = camera_translation.x - window_half_width;
@@ -176,18 +165,16 @@ pub fn spawn_platform(
     exclusions.sort_unstable_by(|a, b| a.lower.partial_cmp(&b.lower).unwrap());
     let region_set = RegionSet::with_sorted(left_of_camera, right_of_camera, exclusions);
     if let Some(spawn_region) = region_set.random() {
-        let x_coord = rand::thread_rng().gen_range(spawn_region.lower..=spawn_region.upper);
+        let x_coord = rand::rng().random_range(spawn_region.lower..=spawn_region.upper);
         commands.spawn((
-            MaterialMesh2dBundle {
-                mesh: meshes.add(Rectangle::new(200., 200.)).into(),
-                transform: Transform::from_xyz(x_coord, top_of_camera, 2.),
-                material: materials.add(CloudMaterial {
+            (Mesh2d (meshes.add(Rectangle::new(200., 200.))),
+                MeshMaterial2d(materials.add(CloudMaterial {
                     blue_noise: Some(asset_server.load("noise/blue.png")),
                     perlin_noise: Some(asset_server.load("noise/perlin.png")),
-                    seed: rand::thread_rng().gen(),
-                }),
-                ..default()
-            },
+                    seed: rand::rng().random(),
+                })),
+                Transform::from_xyz(x_coord, top_of_camera, 2.),
+            ),
             Ground,
         ));
     }
